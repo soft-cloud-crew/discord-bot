23f682d34b87ab5b115b551a96f5c85e6a745bd4
Ripazha1333 <nhada1333@gmail.com>
2024-01-28 13:48:53 -0600

    fixes for mine.py

--- mine.py
+++ mine.py
@@ -4,18 +4,19 @@ from discord.ext import commands
 class Minecraft( commands.Cog ):
     def __init__( self, bot ):
         self.bot = bot
-        self.server = JavaServer( "scc-server.duckdns.org" )
+        self.server = JavaServer( "scc-server.duckdns.org", 25565 )
 
-    @command.hybrid_group( fallback="ping" )
+    @commands.hybrid_group( fallback="ping" )
     async def mine( self, ctx ):
-        await ctx.send( f"el servidor ha respondido en {self.server.ping()} ms." )
+        await ctx.send( f"el servidor ha respondido en {await self.server.async_ping()} ms." )
 
-    @mine.hybrid_command( )
+    @mine.command( )
     async def list( self, ctx ):
-        query = self.server.query()
+        query = await self.server.async_query()
         lista = query.players.names
-        await ctx.send( f"{len(lista)} usuarios conectados:\n{'\n'.join(lista)}" )
+        flista = '\n'.join( lista )
+        await ctx.send( f"{len(lista)} usuarios conectados:\n{ flista }" )
 
 async def setup( bot ):
-    bot.add_cog( Minecraft( bot ) )
-    print("Funcionaliiad para servidor de minecraft")
+    await bot.add_cog( Minecraft( bot ) )
+    print("Funcionalidad para servidor de minecraft")
