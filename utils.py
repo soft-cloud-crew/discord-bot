0c585c543dd3db0dadaa0a48f2bb6f4933906c3f
Ripazha1333 <nhada1333@gmail.com>
2024-06-23 14:25:34 -0600

    terminada la utilidad de tiempo por el momento

--- utils.py
+++ utils.py
@@ -4,6 +4,107 @@ import discord
 from discord.ext import commands
 
 
+def time_text( unix_time ):
+    template = "{0:12}\n<t:{1}:{2}> \\<t:{1}:{2}>\n\n"
+
+    text  = template.format( "Relativo:",     unix_time, "R" )
+    text += template.format( "Hora corta:",   unix_time, "t" )
+    text += template.format( "Dia corto:",    unix_time, "d" )
+    text += template.format( "Dia largo:",    unix_time, "D" )
+    text += template.format( "Tiempo corto:", unix_time, "f" )
+    text += template.format( "Tiempo largo:", unix_time, "F" )
+
+    return text
+
+
+
+class TimeMenu(discord.ui.View):
+
+
+    async def add_time( self, interaction, add_time ):
+        org_embed = interaction.message.embeds[0]
+        time = int( org_embed.footer.text ) + add_time
+
+        title = "Tiempo actual"
+        description = time_text( time )
+
+        embed = discord.Embed( title=title, description=description )
+        embed.set_footer( text=time )
+
+        await interaction.response.edit_message( embed=embed )
+
+
+    @discord.ui.button( label="-1m", style=discord.ButtonStyle.red )
+    async def subtract_minute( self, interaction, button ):
+        await self.add_time( interaction, -60 )
+
+
+    @discord.ui.button( label="+1m", style=discord.ButtonStyle.green )
+    async def add_minute( self, interaction, button ):
+        await self.add_time( interaction, 60 )
+
+
+    @discord.ui.button( label="-10m", style=discord.ButtonStyle.red )
+    async def subtract_ten_minute( self, interaction, button ):
+        await self.add_time( interaction, -60*10 )
+
+
+    @discord.ui.button( label="+10m", style=discord.ButtonStyle.green )
+    async def add_ten_minute( self, interaction, button ):
+        await self.add_time( interaction, 60*10 )
+
+
+    @discord.ui.button( label="-1h", style=discord.ButtonStyle.red )
+    async def subtract_hour( self, interaction, button ):
+        await self.add_time( interaction, -3600 )
+
+
+    @discord.ui.button( label="+1h", style=discord.ButtonStyle.green )
+    async def add_hour( self, interaction, button ):
+        await self.add_time( interaction, 3600 )
+
+
+    @discord.ui.button( label="-1d", style=discord.ButtonStyle.red )
+    async def subtract_day( self, interaction, button ):
+        await self.add_time( interaction, -3600*24 )
+
+
+    @discord.ui.button( label="+1d", style=discord.ButtonStyle.green )
+    async def add_day( self, interaction, button ):
+        await self.add_time( interaction, 3600*24 )
+
+
+    @discord.ui.button( label="-7d", style=discord.ButtonStyle.red )
+    async def subtract_week( self, interaction, button ):
+        await self.add_time( interaction, -3600*24*7 )
+
+
+    @discord.ui.button( label="+7d", style=discord.ButtonStyle.green )
+    async def add_week( self, interaction, button ):
+        await self.add_time( interaction, 3600*24*7 )
+
+
+    @discord.ui.button( label="-28d", style=discord.ButtonStyle.red )
+    async def subtract_month( self, interaction, button ):
+        await self.add_time( interaction, -3600*24*28 )
+
+
+    @discord.ui.button( label="+28d", style=discord.ButtonStyle.green )
+    async def add_month( self, interaction, button ):
+        await self.add_time( interaction, 3600*24*28 )
+
+
+    @discord.ui.button( label="-365d", style=discord.ButtonStyle.red )
+    async def subtract_year( self, interaction, button ):
+        await self.add_time( interaction, -3600*24*365 )
+
+
+    @discord.ui.button( label="+365d", style=discord.ButtonStyle.green )
+    async def add_year( self, interaction, button ):
+        await self.add_time( interaction, 3600*24*365 )
+
+
+
 class Utils( commands.Cog ):
     def __init__( self, bot ):
         self.bot = bot
@@ -59,6 +160,19 @@ class Utils( commands.Cog ):
     @responder.autocomplete( 'codigo' )
     async def responder_autocomplete( self, interaction, curr ):
         return [discord.app_commands.Choice(name=self.cuestionarios[x]['title'],value=x) for x in self.cuestionarios][:25]
+
+
+    @commands.hybrid_group( fallback='actual' )
+    async def tiempo( self, ctx ):
+        current_time = int( time.time( ) )
+
+        title = "Tiempo actual"
+        description = time_text( current_time )
+
+        embed = discord.Embed( title=title, description=description )
+        embed.set_footer( text=current_time )
+
+        await ctx.send( embed=embed, view=TimeMenu() )
 
 
 
